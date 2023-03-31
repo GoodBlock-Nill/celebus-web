e364c9b78eab079a7a6978f613ba5c0f0e35d9c0
GoodBlock-Nill <nill@good-block.com>
2023-03-31 11:05:30 +0900

    스크린9, 푸터 완성

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Container, Grid, Typography, Box } from '@mui/material';
+import { Container, Grid, Typography, Box, Link, IconButton, Button } from '@mui/material';
 import styles from './AppFooter.module.css';
+import appStore from '../../assets/image/AppStore.png';
+import googleStore from '../../assets/image/GooglePlay.png';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const AppFooter = () => {
   return (
@@ -9,12 +13,18 @@ const AppFooter = () => {
         <Grid container justifyContent="space-between" alignItems="center">
           <Grid item xs={12} md={4}>
             <Box className={styles.footerLeft}>
-              <a href="#" className={styles.appStoreButton}>
-                <img src="/path/to/app-store-icon.png" alt="App Store" />
-              </a>
-              <a href="#" className={styles.googlePlayButton}>
-                <img src="/path/to/google-play-icon.png" alt="Google Play" />
-              </a>
+              {/* App Store download button */}
+              <Link href="https://apps.apple.com/app/idYOUR_APP_ID" target="_blank" rel="noopener noreferrer">
+                <Button>
+                  <img src={appStore} alt="AppStore" className={styles.storeImg} />
+                </Button>
+              </Link>
+              {/* Google Play download button */}
+              <Link href="https://play.google.com/store/apps/details?id=YOUR_PACKAGE_NAME" target="_blank" rel="noopener noreferrer">
+                <Button>
+                  <img src={googleStore} alt="GooglePlay" className={styles.storeImg} />
+                </Button>
+              </Link>
             </Box>
           </Grid>
           <Grid item xs={12} md={4}>
@@ -24,19 +34,27 @@ const AppFooter = () => {
           </Grid>
           <Grid item xs={12} md={4}>
             <Box className={styles.footerRight}>
-              <a href="/privacy-policy" className={styles.linkButton}>
-                Privacy Policy
-              </a>
-              <a href="/terms-of-service" className={styles.linkButton}>
-                Terms of Service
-              </a>
+              <Box className={styles.terms}>
+                <Button className={styles.linkButton} href="/privacy-policy" target="_blank">
+                    Privacy Policy
+                </Button>
+                <Button className={styles.linkButton} href="/privacy-policy" target="_blank">
+                    Terms of service
+                </Button>
+              </Box>
               <Box className={styles.socialIcons}>
-                <a href="#" className={styles.socialButton}>
-                  <img src="/path/to/twitter-icon.png" alt="Twitter" />
-                </a>
-                <a href="#" className={styles.socialButton}>
-                  <img src="/path/to/youtube-icon.png" alt="YouTube" />
-                </a>
+                {/* Twitter Link button */}
+                <Link href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                  <IconButton>
+                    <TwitterIcon />
+                  </IconButton>
+                </Link>
+                {/* Youtube Link button */}
+                <Link href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+                  <IconButton>
+                    <YouTubeIcon />
+                  </IconButton>
+                </Link>
               </Box>
             </Box>
           </Grid>
--- src/components/Screens/Screen9.js
+++ src/components/Screens/Screen9.js
@@ -1,28 +1,34 @@
+// Screen9.js
 import React from 'react';
 import { Container, Grid, Box, Typography, IconButton, Link } from '@mui/material';
 import styles from './Screen9.module.css';
+import { Fade } from 'react-awesome-reveal';
 import iphoneImage from '../../assets/image/screen9.png';
 import s9Logo from '../../assets/image/screen9Logo.png';
 import appleStore from '../../assets/image/AppStore2.png';
 import googleStore from '../../assets/image/GooglePlay2.png';
+import { FormattedMessage } from 'react-intl';
+
 
 const Screen9 = () => {
   return (
     <Box className={styles.screen9Container}>
       <Container maxWidth="lg">
         <Grid container spacing={3} alignItems="center">
-          <Grid item xs={12} md={6}>
+          {/* Left group: Start Now text and app download buttons */}
+          <Grid item xs={12} md={8}>
             <Box className={styles.leftGroup}>
               <Typography variant="h2" className={styles.startNowText} color='white' fontWeight={700}>
-                Start Now!
+                <FormattedMessage id="screen9.startNow" defaultMessage="Start Now!" />
               </Typography>
               <Box className={styles.downloadButtons}>
-                {/* Replace the '#' in the href attribute with the appropriate app store links */}
+                {/* App Store download button */}
                 <Link href="https://apps.apple.com/app/idYOUR_APP_ID" target="_blank" rel="noopener noreferrer">
                   <IconButton>
                     <img src={appleStore} alt="AppStore" />
                   </IconButton>
                 </Link>
+                {/* Google Play download button */}
                 <Link href="https://play.google.com/store/apps/details?id=YOUR_PACKAGE_NAME" target="_blank" rel="noopener noreferrer">
                   <IconButton>
                     <img src={googleStore} alt="GooglePlay" />
@@ -31,11 +37,14 @@ const Screen9 = () => {
               </Box>
             </Box>
           </Grid>
-          <Grid item xs={12} md={6}>
+          {/* Right group: iPhone image with rotating logo */}
+          <Grid item xs={12} md={4}>
+           <Fade direction="right">
             <Box className={styles.rightGroup}>
               <img src={iphoneImage} alt="iPhone" className={styles.iphoneImage} />
-              <img src={s9Logo} alt="Logo" className={styles.s9logo} />
+              <img src={s9Logo} alt="Logo" className={`${styles.s9logo} ${styles.imageFloating}`} />
             </Box>
+           </Fade>
           </Grid>
         </Grid>
       </Container>
